b8e2410f32dcdd0ac5eaea76b33ae55f86d18517
VladShag <126063712+VladShag@users.noreply.github.com>
2023-07-04 13:06:31 +0300

    Logger added

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,9 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Server {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class.getName());
     private static Socket clientSocket;
     private static ServerSocket server;
     private static final ArrayList<UserHandler> userList = new ArrayList<>();
@@ -13,10 +18,12 @@ public class Server {
         try {
             server = new ServerSocket(PORT);
             System.out.println("Server started!");
+            logger.info("Server started at {}", LocalDateTime.now());
             try {
                 while (true) {
                     clientSocket = server.accept();
                     System.out.println("New client accepted!");
+                    logger.info("New user connected: {}", clientSocket);
                     UserHandler user = new UserHandler(this, clientSocket);
                     userList.add(user);
                 }
@@ -28,6 +35,7 @@ public class Server {
         } finally {
             try {
                 server.close();
+                logger.info("Server closed at {}", LocalDateTime.now());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
--- src/main/java/UserHandler.java
+++ src/main/java/UserHandler.java
@@ -1,12 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.Socket;
 
 
 public class UserHandler extends Thread{
+    private static final Logger logger = LoggerFactory.getLogger(UserHandler.class.getName());
     private final Server server;
     private final BufferedReader in;
     private final PrintWriter out;
     private final Socket socket;
+
     public UserHandler(Server server, Socket socket) throws IOException {
         this.socket = socket;
         this.server = server;
@@ -21,17 +26,28 @@ public class UserHandler extends Thread{
     }
     @Override
     public void run() {
+        String newUser;
+        try {
+            newUser = in.readLine();
+            server.sendMessageToEveryone(newUser + " is connected!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         while (true) {
-            String msg;
             try {
-                msg = in.readLine();
-                if (msg.equals("exit")) {
+                String msg = in.readLine();
+                if (msg == null) {
+                    server.sendMessageToEveryone(newUser + " disconnected ");
+                    logger.info("{}, {} disconnected", newUser, socket);
+                    socket.close();
                     break;
                 } else {
+                    logger.info("New message accepted: {}", msg);
                     System.out.println("Echo: " + msg);
                     server.sendMessageToEveryone(msg);
                 }
-            } catch (IOException ignored) {}
+            } catch (IOException ignored) {
+            }
         }
     }
 }
