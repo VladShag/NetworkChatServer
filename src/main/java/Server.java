d99d7b850bc08a9a57fe7720bdc5266c7b22d72f
VladShag <126063712+VladShag@users.noreply.github.com>
2023-07-05 08:48:34 +0300

    ReadSettingsAdded

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,22 +1,25 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Server {
     private static final Logger logger = LoggerFactory.getLogger(Server.class.getName());
     private static Socket clientSocket;
     private static ServerSocket server;
     private static final ArrayList<UserHandler> userList = new ArrayList<>();
-    private static final int PORT = 8080;
 
     public Server() {
+        int port = getPortFromSettings("src/main/resources/settings.txt");
         try {
-            server = new ServerSocket(PORT);
+            server = new ServerSocket(port);
             System.out.println("Server started!");
             logger.info("Server started at {}", LocalDateTime.now());
             try {
@@ -46,6 +49,18 @@ public class Server {
         for (UserHandler user : userList) {
             user.sendMsg(msg);
         }
+    }
+    public static int getPortFromSettings(String filename) {
+        File file = new File(filename);
+        try {
+            Scanner scanner = new Scanner(file);
+            String port = scanner.nextLine();
+            String[] portSplited = port.split(":");
+            return Integer.parseInt(portSplited[1]);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
